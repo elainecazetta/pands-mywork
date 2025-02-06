99775de92c26aa3ef2ce64311a0aea8b5b01b4d5
Admin <elaine.rodrigues1@gmail.com>
2025-02-06 18:54:30 +0000

    week02 task

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+# helloworld.py
+# This program just prints out hello world
+# Author Elaine Cazetta
+
+print("Hello World!")
---
+++ week02/hello.py
@@ -0,0 +1,5 @@
+# hello.py
+# This program just prints out hello
+# Author Elaine Cazetta
+
+print("Hello Elaine")
