f4e5636d75536e1cac259773b6d845e2af884629
elainecazetta <elaine.rodrigues1@gmail.com>
2025-02-16 21:36:00 +0000

    Weekly Task 03

--- week03/accounts.py
+++ week03/accounts.py
@@ -10,15 +10,17 @@
 #
 # Author Elaine Cazetta
 # Source: https://www.w3schools.com/python/python_strings_slicing.asp and ChatGPT (function 'len')
-
+#
 # Answer:
-
-# The input is limited by 10 characters, using the slicer function
-account = input("Please enter an 10 digit account number: ")[:10]
-# Funtion len() and "-4" were used to count the number of the account's characters
-# and subtract it by 4 = the four last digits 
-# the result, 6, is the number of characters that are going to be replaced by Xs 
-hidden_account = "X" * (len(account) - 4) + account[6:]
-print(hidden_account) # prints 6 times the character "X" + the account's four last digits
-
-# Extra: 
+#
+# The input is limited by the first 10 characters, using the slicer function
+account = input("Please enter an 10 digit account number: ") [:10]
+# Funtion ' "X" * (len(account) - 4) ' was used to count the number 
+# of the account's characters, minus 4, and replace it by Xs 
+hidden_account = "X" * (len(account) - 4) + account[-4:]
+print(hidden_account) # prints a number of Xs + the account's four last digits
+#
+# Extra Task - Answer:
+# To handle any length, I would remove the slicer "[:10]"
+# then if the account number has 4 or fewer digits, it'll show as is
+# if it’s longer, the program will mask all but the last 4 digits
