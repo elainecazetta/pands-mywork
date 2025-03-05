ce79232f9f8b4136773254ecaf30ef2428910895
elainecazetta <elaine.rodrigues1@gmail.com>
2025-03-05 11:54:37 +0000

    week06 lab

---
+++ week06/lab6.4_doAdd.py
@@ -0,0 +1,17 @@
+def readModules():
+    return []
+
+
+def doAdd(students):
+    currentStudent = {}
+    currentStudent["name"]=input("enter name :")
+    currentStudent["modules"]= readModules()
+
+    students.append(currentStudent)
+
+
+#test
+students = []
+doAdd(students)
+doAdd(students)
+print(students)
