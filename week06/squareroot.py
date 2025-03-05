1c2ec84692d48be1bb66f9e7ce9a5372d273a005
elainecazetta <elaine.rodrigues1@gmail.com>
2025-03-05 14:57:46 +0000

    week06 lab

---
+++ week06/squareroot.py
@@ -0,0 +1,13 @@
+def sqrt(number):
+    guess = number / 2.0
+    tolerance = 0.0001
+
+    while abs(guess * guess - number) > tolerance:
+        guess = (guess + number / guess) / 2
+
+    return guess
+
+num = float(input("Please enter a positive number: "))
+result = sqrt(num)
+
+print(f"The square root of {num} is approx. {result:.1f}")
