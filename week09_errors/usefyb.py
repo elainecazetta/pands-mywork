9a4f1b5e19b819b43942ce364925e2ad4fdd13df
elainecazetta <elaine.rodrigues1@gmail.com>
2025-04-14 10:24:11 +0000

    week09

---
+++ week09_errors/usefyb.py
@@ -0,0 +1,10 @@
+# Author: Andrew Beatty
+# Reproduced by: Elaine Cazetta
+
+# this program prompts the user for a number and 
+# prints out the fibonacci sequence of that many numbers
+
+import myfunctions
+
+nTimes = int(input('how many:'))
+print(myfunctions.fibonacci(nTimes))
